f85e7d9ab789409e942df7ee27d545a0c36e1484
Robert Thompson <robertkthompson@cmail.carleton.ca>
2018-03-25 13:03:03 -0400

    simulation_demo.py should actually be in a working state now.
    Fixed up a problem in an interation.

--- simulation_demo.py
+++ simulation_demo.py
@@ -40,16 +40,17 @@ size_y = 100
 nodes = []
 
 try:
-    path = sys.argv[1]
+	path = sys.argv[1]
 except IndexError:
-    print("No Path given. Try something like this:")
-    print(">> python simulation_demo.py Simulations/radioTest/")
-    pygame.quit()
-    sys.exit()
+	print("No Path given. Try something like this:")
+	print(">> python simuation_demo.py Simulations/radioTest/")
+	pygame.quit()
+	sys.exit()
 
 
 blue = pygame.Color(0,0,255)
 white = pygame.Color(255,255,255)
+
 
 
 ### Read omnetpp.ini file to get field size ###
@@ -57,16 +58,16 @@ inifile = path + 'omnetpp.ini'
 ini = open(inifile, 'r')
 regex = re.compile(r"(\d+)")
 for line in ini:
-    if "SN.field_x" in line:
-        match = regex.search(line)
-        size_x = int(match.group(1))
-        print ("size_x set to %d" %size_x)
-    elif "SN.field_y" in line:
-        match = regex.search(line)
-        size_y = int(match.group(1))
-        print ("size_y set to %d" %size_y)
-    # elif "SN.numNodes" in line:
-    #     match = regex.match(line)
+	if "SN.field_x" in line:
+		match = regex.search(line)
+		size_x = int(match.group(1))
+		print ("size_x set to %d" %size_x)
+	elif "SN.field_y" in line:
+		match = regex.search(line)
+		size_y = int(match.group(1))
+		print ("size_y set to %d" %size_y)
+	# elif "SN.numNodes" in line:
+	# 	match = regex.match(line)
 ini.close()
 
 
@@ -76,56 +77,55 @@ f = open(tracefile, "r")
 regex = re.compile(r"([0-9]+[\.[0-9]*]*)\D*([0-9]+)\D*([0-9]+[\.[0-9]*]*):([0-9]+[\.[0-9]*]*):([0-9]+[\.[0-9]*]*)")
 
 
+
 ### parse a line from the trace file and check for mobility ###
 def parseline(f):
-    line = f.readline()
-    if "initial location" in line:
-        match = regex.match(line)
-        if match:
-            # print match.group(2)
-            # print str(match.group(1)) + ", " + str(match.group(2)) + ", " 
-            # + str(match.group(3)) + ":" + str(match.group(4)) + ":" + 
-            # str(match.group(5))
-            node = int(match.group(2))
-            (x,y) = (int(float(match.group(3))), int(float(match.group(4))))
-            # print node, x, y
-            new_node = (match.group(2),(match.group(3), match.group(4)))
-            nodes.append(new_node)
-            
-            if "changed location" in line:
-                match = regex.match(line)
-                if match:
-                    node = int(match.group(2))
-                    (x,y) = (int(float(match.group(3))),
-                     int(float(match.group(4))))
-                     # print node, x, y
-                    nodes[node] = (node, (x,y))
-                    return nodes
+	line = f.readline()
+	if "initial location" in line:
+		match = regex.match(line)
+		if match:
+			# print match.group(2)
+			# print str(match.group(1)) + ", " + str(match.group(2)) + ", " + str(match.group(3)) + ":" + str(match.group(4)) + ":" + str(match.group(5))
+			node = int(match.group(2))
+			(x,y) = (int(float(match.group(3))), int(float(match.group(4))))
+			# print node, x, y
+			new_node = (match.group(2),(match.group(3), match.group(4)))
+			nodes.append(new_node)
+
+	if "changed location" in line:
+  		match = regex.match(line)
+  		if match:
+	  		node = int(match.group(2))
+	  		(x,y) = (int(float(match.group(3))), int(float(match.group(4))))
+	  		# print node, x, y
+	  		nodes[node] = (node, (x,y))
+
+	return nodes
 
 
 ### Main loop of "Game" ###
 while True:
-    nodes = parseline(f)
-    screen.fill(white)
+	nodes = parseline(f)
+	screen.fill(white)
 
-    # print nodes
+	# print nodes
 
-    for node in nodes:
-        # print "node " + str(node)
-        # print node[1][0]
-        x = int(int(node[1][0]) * res_x/size_x)
-        y = int(int(node[1][1]) * res_y/size_y)
-        pygame.draw.circle(screen, blue, (x,y), 10, 0)
+	for node in nodes:
+		# print "node " + str(node)
+		# print node[1][0]
+		x = int(int(node[1][0]) * res_x/size_x)
+		y = int(int(node[1][1]) * res_y/size_y)
+		pygame.draw.circle(screen, blue, (x,y), 10, 0)
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            f.close()
-            sys.exit()
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			f.close()
+			sys.exit()
 
-        elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                pygame.event.post(pygame.event.Event(QUIT))
+		elif event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
+				pygame.event.post(pygame.event.Event(QUIT))
 
-    pygame.display.update()
-    fpsClock.tick(30)
+	pygame.display.update()
+fpsClock.tick(30)
